825f1f010efa96db0d5206aab026528c677c1003
Ko-Phi <172218929+Ko-Phi@users.noreply.github.com>
2026-07-04 20:37:56 -0700

    Correct Lambda
    Identifiers stored as themselves rather than as strings

--- lambda.hs
+++ lambda.hs
@@ -1,12 +1,11 @@
 import Control.Applicative
-import Control.Monad (ap)
 import Data.Char
 import Data.List
 
 data Expression
   = Identifier String
   | Application Expression Expression
-  | Lambda String Expression
+  | Lambda Expression Expression -- Where first expression is an identifier
   deriving (Eq)
 
 instance Show Expression where
@@ -15,7 +14,7 @@ instance Show Expression where
   show (Application l@(Lambda _ _) e2) = "(" ++ show l ++ ")(" ++ show e2 ++ ")"
   show (Application e1 (Identifier i2)) = show e1 ++ " " ++ i2
   show (Application e1 e2) = show e1 ++ " (" ++ show e2 ++ ")"
-  show (Lambda s e) = "λ" ++ s ++ "." ++ show e
+  show (Lambda s e) = "λ" ++ show s ++ "." ++ show e
 
 newtype Parser a = Parser
   { runParser :: String -> Maybe (String, a)
@@ -48,12 +47,7 @@ instance Alternative Parser where
   p1 <|> p2 = Parser $ \s -> runParser p1 s <|> runParser p2 s
 
 char :: Char -> Parser Char
-char c =
-  Parser $ \s -> do
-    (x, xs) <- uncons s
-    if x == c
-      then return (xs, c)
-      else Nothing
+char c = charIf (== c)
 
 charIf :: (Char -> Bool) -> Parser Char
 charIf p =
@@ -66,12 +60,6 @@ charIf p =
 string :: String -> Parser String
 string = traverse char
 
-spanP :: (Char -> Bool) -> Parser String
-spanP p =
-  Parser $ \s ->
-    let (token, rest) = span p s
-     in return (rest, token)
-
 notNull :: Parser [a] -> Parser [a]
 notNull p =
   Parser $ \s -> do
@@ -81,10 +69,10 @@ notNull p =
       else return (s', xs)
 
 ws :: Parser String
-ws = spanP isSpace
+ws = many $ charIf isSpace
 
 forceWs :: Parser String
-forceWs = notNull $ spanP isSpace
+forceWs = some $ charIf isSpace
 
 sepBy :: Parser a -> Parser b -> Parser [b]
 sepBy sep element = (:) <$> element <*> many (sep *> element) <|> pure []
@@ -92,7 +80,7 @@ sepBy sep element = (:) <$> element <*> many (sep *> element) <|> pure []
 identifier :: Parser Expression
 identifier = do
   c <- charIf isAlpha
-  cs <- spanP isAlphaNum
+  cs <- many $ charIf isAlphaNum
   return $ Identifier (c : cs)
 
 grouping :: Parser Expression
@@ -113,10 +101,9 @@ lambda :: Parser Expression
 lambda = do
   (char '\\' <|> char 'λ') >> ws
   is <- notNull $ sepBy forceWs identifier
-  ws >> (string "->" <|> singleton <$> char '.') >> ws
+  ws >> (string "->" <|> pure <$> char '.') >> ws
   e <- expression
-  let is' = unwrapId <$> is
-  return $ foldr Lambda (Lambda (last is') e) (init is')
+  return $ foldr Lambda (Lambda (last is) e) (init is)
 
 notApplication :: Parser Expression
 notApplication = lambda <|> grouping <|> identifier
@@ -135,7 +122,7 @@ replace s e1 (Application e2 e3) =
 replace s e1 (Lambda i1 e2) = Lambda i1 (replace s e1 e2)
 
 reduce :: Expression -> Expression
-reduce (Application (Lambda i e1) e2) = replace i e2 e1
+reduce (Application (Lambda (Identifier i) e1) e2) = replace i e2 e1
 reduce x = x
 
 apply e1 e2 = reduce (Application e1 e2)
